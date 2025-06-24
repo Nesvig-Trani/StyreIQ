809523a6e00f8cf238e00e02d5f8aae0dc36cb90
Leider Escobar <leider.escobar@meltstudio.co>
2025-06-24 17:05:15 -0500

    initial migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,1 +1,9 @@
-export const migrations = []
+import * as migration_20250624_215652 from './20250624_215652';
+
+export const migrations = [
+  {
+    up: migration_20250624_215652.up,
+    down: migration_20250624_215652.down,
+    name: '20250624_215652'
+  },
+];
