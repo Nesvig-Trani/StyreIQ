185aa4b3ff6c9e0e016701730460019d5281006a
Leider Escobar <leider.escobar@meltstudio.co>
2025-07-10 08:46:38 -0500

    fix(server): pending migration, social media, audit logs, jobs

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20250624_215652 from './20250624_215652'
 import * as migration_20250702_230347 from './20250702_230347'
 import * as migration_20250708_164711_flags_comments_history from './20250708_164711_flags_comments_history'
+import * as migration_20250710_134307 from './20250710_134307'
 
 export const migrations = [
   {
@@ -17,5 +18,10 @@ export const migrations = [
     up: migration_20250708_164711_flags_comments_history.up,
     down: migration_20250708_164711_flags_comments_history.down,
     name: '20250708_164711_flags_comments_history',
+  },
+  {
+    up: migration_20250710_134307.up,
+    down: migration_20250710_134307.down,
+    name: '20250710_134307',
   },
 ]
