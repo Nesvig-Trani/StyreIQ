ae2cbc9733da53e16428f947f29a15787e5877a8
sebasmelt <sebastian@meltstudio.co>
2025-08-06 15:08:06 -0500

    fix: updated github actions

--- .github/.lintstagedrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  '**/*.{js,jsx,ts,tsx,hbs,html,mjml,css,scss,json,graphql,yml,yaml,md}': [
-    'prettier --write',
-  ],
-  '{.prettierrc,.eslintrc,.releaserc,.commitlintrc}': ['prettier --write'],
-  '**/*.{ts,tsx}': ['yarn eslint --max-warnings=0 --fix'],
-  '**/package.json': (filenames) =>
-    `yarn syncpack format --source ${filenames.join(' --source ')}`,
-};
