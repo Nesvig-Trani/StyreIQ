290982c8369878b9c36a1e9d4095d4016616d37c
Hilary <hilary@meltstudio.co>
2026-01-21 11:19:58 -0500

    fix: cleanup Iubenda widget on landing page unmount

--- src/types/payload-types.ts
+++ src/types/payload-types.ts
@@ -513,21 +513,15 @@ export interface SocialMediaPost {
 export interface Policy {
   id: number
   version?: number | null
-  text?: {
-    root: {
-      type: string
-      children: {
-        type: string
-        version: number
+  text?:
+    | {
         [k: string]: unknown
-      }[]
-      direction: ('ltr' | 'rtl') | null
-      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | ''
-      indent: number
-      version: number
-    }
-    [k: string]: unknown
-  } | null
+      }
+    | unknown[]
+    | string
+    | number
+    | boolean
+    | null
   author?: (number | null) | User
   tenant?: (number | null) | Tenant
   updatedAt: string
